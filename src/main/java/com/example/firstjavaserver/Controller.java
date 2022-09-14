9c091c65186f92f167b9e0b6fb286e25f839076e
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2022-09-13 22:51:29 -0500

    FIXES sonarcloud smells

--- src/main/java/com/example/firstjavaserver/Controller.java
+++ src/main/java/com/example/firstjavaserver/Controller.java
@@ -2,15 +2,17 @@ package com.example.firstjavaserver;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 @RestController
 public class Controller {
 
     @GetMapping
     static String helloDevops(){
-        System.out.println("Hello Devops!");
+        //System.out.println("Hello Devops!");
+        Logger logger = Logger.getLogger("mess::  ");
+        logger.log(Level.INFO, "Message to log");
         return "Hello Devops!";
     }
 
